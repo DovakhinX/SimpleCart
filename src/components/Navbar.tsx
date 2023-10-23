e799b6bdc0b5af8012e5eed4ae4d38b999c1e135
DovakhinX <sunwin60@gmail.com>
2023-10-23 15:29:19 +0530

    Pagination added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,22 @@
 import logo from '../assets/android.svg'
 import cart from '../assets/cart.svg'
+import cart2 from '../assets/cart-outline.svg'
 import { Link } from 'react-router-dom'
+import { useCartStore } from '../stores/CartStore'
 
 const Navbar = () => {
+
+  const { cartItems } = useCartStore()
 
   return (
     <nav className='w-full bg-yellow-300 h-14 px-5 flex flex-row justify-between items-center border-b border-black'>
       <Link to={'/'}>
         <img src={logo} width={150} ></img>
       </Link>
-      <Link to={'/cart'}>
-        <img src={cart} width={30} ></img>
+      <Link to={'/cart'}>{
+        cartItems.length > 0 ?
+          <img src={cart} width={30} className='shaker' ></img> : <img src={cart2} width={30} ></img>
+      }
       </Link>
     </nav>
   )
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,7 @@
 import Card from '../components/Card'
 import Navbar from '../components/Navbar'
 import { useState, useEffect } from 'react'
-import  {useCartStore} from '../stores/CartStore'
+import { useCartStore } from '../stores/CartStore'
 
 export interface Product {
     id: number;
@@ -14,12 +14,17 @@ export interface Product {
     thumbnail: string;
     description: number;
     discountPercentage: number;
-    quantity:number
+    quantity: number
 }
 const Homepage = () => {
 
     const [data, setData] = useState<Product[]>([])
-    const{addToCart}=useCartStore()
+    const [curPage, setCurPage] = useState(1)
+    const { addToCart } = useCartStore()
+    const itemPerPage = 5
+    const totalItems = data?.length ?? 30
+    const totalPages = Math.ceil(totalItems / itemPerPage)
+    const pageRange = 3
 
     useEffect(() => {
         fetch('https://dummyjson.com/products')
@@ -28,7 +33,7 @@ const Homepage = () => {
             .catch(error => console.error(error))
     }, [])
 
-
+    const paginatedData = data?.slice((curPage - 1) * itemPerPage, curPage * itemPerPage)
 
     return (
         <>
@@ -36,7 +41,7 @@ const Homepage = () => {
             <main className='flex flex-col items-center pt-6'>
                 <h1 className='font-bold font-mono text-2xl text-orange-400 '>Spring Sales !!</h1>
                 <div className='flex flex-row gap-4 flex-wrap my-5 px-1 justify-center'>
-                    {data?.map(product => {
+                    {paginatedData?.map(product => {
                         return (
                             <Card
                                 key={product.id}
@@ -44,11 +49,23 @@ const Homepage = () => {
                                 thumbnail={product.thumbnail}
                                 id={product.id}
                                 price={product.price}
-                                onClick={()=>addToCart(product)}
-
-                                />
+                                onClick={() => addToCart(product)}
+                            />
                         )
                     })}
+                </div>
+                <div className='flex flex-row gap-4'>
+                    {curPage > 1 && <button onClick={() => setCurPage(curPage - 1)}>-</button>}
+                    {Array.from(Array(totalPages).keys())
+                        .slice(Math.max(curPage - Math.floor(pageRange / 2), 0),
+                            Math.min(curPage + Math.floor(pageRange / 2) + 1, totalPages))
+                        .map(page => (
+                            <button onClick={() => setCurPage(page + 1)} className='border-2 border-black rounded-full px-2 '
+                                style={{ background: curPage === page + 1 ? 'orange' : 'bisque' }}
+                                key={page}
+                            >{page + 1}</button>
+                        ))}
+                    {curPage < totalPages && <button onClick={() => { setCurPage(curPage + 1) }}>+</button>}
                 </div>
             </main>
         </>
